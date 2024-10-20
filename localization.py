95971aa8cf8a59437e027410176541984d6afc90
Hunter McCullagh <76576376+HunterMcCullagh@users.noreply.github.com>
2024-10-20 14:15:19 -0400

    added odometry subscription

--- localization.py
+++ localization.py
@@ -1,4 +1,5 @@
 import sys
+import rclpy
 
 from utilities import Logger, euler_from_quaternion
 from rclpy.time import Time
@@ -19,22 +20,26 @@ class localization(Node):
         # TODO Part 3: Define the QoS profile variable based on whether you are using the simulation (Turtlebot 3 Burger) or the real robot (Turtlebot 4)
         # Remember to define your QoS profile based on the information available in "ros2 topic info /odom --verbose" as explained in Tutorial 3
 
-        odom_qos=...
+        odom_qos=QoSProfile(reliability=2, durability=2, history=1, depth=10) # create QoS profile based on tutorial
         
         self.loc_logger=Logger("robot_pose.csv", ["x", "y", "theta", "stamp"])
         self.pose=None
         
         if localizationType == rawSensor:
         # TODO Part 3: subscribe to the position sensor topic (Odometry)
-        # ...
+            self.create_subscription(odom, "/odom", self.odom_callback, qos_profile=odom_qos)
         else:
             print("This type doesn't exist", sys.stderr)
     
     
     def odom_callback(self, pose_msg):
+        timestamp = str(Time.from_msg(pose_msg.header.stamp).nanoseconds) # extract timestamp in nanoseconds from the odom_msg
+        odom_yaw = str(euler_from_quaternion(pose_msg.pose.pose.orientation)) # extract orientation as a quaternion from the odom_msg object and convert to a yaw angle
+        odom_x_pos = str(pose_msg.pose.pose.position.x) # extract x position from the odom_msg object
+        odom_y_pos = str(pose_msg.pose.pose.position.y) # extract y position from the odom_msg object
         
         # TODO Part 3: Read x,y, theta, and record the stamp
-        self.pose=[ ... ]
+        self.pose=[odom_x_pos,odom_y_pos, odom_yaw, timestamp]
         
         # Log the data
         self.loc_logger.log_values([self.pose[0], self.pose[1], self.pose[2], Time.from_msg(self.pose[3]).nanoseconds])
