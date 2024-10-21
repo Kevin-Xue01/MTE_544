554bbc1aa41fd51b3db8ce3cd3661e230ce37a45
Hunter McCullagh <76576376+HunterMcCullagh@users.noreply.github.com>
2024-10-20 20:43:40 -0400

    added trajectory planner function

--- planner.py
+++ planner.py
@@ -1,7 +1,11 @@
+import math
 # Type of planner
 POINT_PLANNER=0; TRAJECTORY_PLANNER=1
 
-
+PARABOLA = 0
+SIGMOID = 1
+selected_trajectory = PARABOLA
+trajectory_resolution = 100
 
 class planner:
     def __init__(self, type_):
@@ -25,7 +29,22 @@ class planner:
 
     # TODO Part 6: Implement the trajectories here
     def trajectory_planner(self):
-        pass
-        # the return should be a list of trajectory points: [ [x1,y1], ..., [xn,yn]]
-        # return 
+        trajectory = []
+        x_maximum = 2.5
+        for x_step in range(int(trajectory_resolution*x_maximum)):
+            x = x_step/trajectory_resolution
 
+            if(selected_trajectory == PARABOLA):
+                y = (x*x)
+            elif(selected_trajectory == SIGMOID):
+                y = 2/(1+math.exp(-2*x))
+
+            trajectory.append([x,y])
+            #print([x,y])
+        # the return should be a list of trajectory points: [ [x1,y1], ..., [xn,yn]]
+        return trajectory
+
+#used this to test trajectory plkanner
+# if __name__=="__main__":
+#     planner_class = planner(type_ = 1)
+#     planner_class.trajectory_planner()
