d029343896ad8aacbdc4cd40dfe90b394fe67281
kevin <kevinx1698@gmail.com>
2024-10-22 21:48:32 -0400

    PID ctrl update

--- localization.py
+++ localization.py
@@ -35,7 +35,7 @@ class localization(Node):
         
         self.pose=[odom_x_pos, odom_y_pos, odom_yaw, timestamp]
         
-        self.loc_logger.log_values([self.pose[0], self.pose[1], self.pose[2], Time.from_msg(self.pose[3]).nanoseconds])
+        self.loc_logger.log_values([self.pose[0], self.pose[1], self.pose[2], self.pose[3]])
 
     def getPose(self):
         return self.pose
--- pid.py
+++ pid.py
@@ -1,8 +1,6 @@
-from rclpy.time import Time
 from utilities import ControllerGains, Config, ControllerType, Logger
 
 class PID_ctrl:
-    
     def __init__(self, config: dict[ControllerGains, float], filename, history_length=3):
         self.history_length=history_length
         self.history=[]
@@ -14,8 +12,6 @@ class PID_ctrl:
         self.ki=config[ControllerGains.KI] # integral gain
         
         self.logger=Logger(filename + "_errors.csv")
-        # Remeber that you are writing to the file named filename_ or errors.csv the following:
-            # error, error_dot, error_int and time stamp
     
     def update(self, stamped_error, status):
         if status == False:
@@ -26,8 +22,8 @@ class PID_ctrl:
 
         
     def __update(self, stamped_error):
-        latest_error=stamped_error[0]
-        stamp=stamped_error[1]
+        latest_error = stamped_error[0]
+        latest_error_timestamp = stamped_error[1]
         
         self.history.append(stamped_error)        
         
@@ -38,53 +34,35 @@ class PID_ctrl:
         if (len(self.history) != self.history_length):
             return self.kp * latest_error
         
-        # Compute the error derivative
-        dt_avg=0
-        error_dot=0
+        dt_avg = 0
+        error_dot = 0
         
         for i in range(1, len(self.history)):
             
-            t0=Time.from_msg(self.history[i-1][1])
-            t1=Time.from_msg(self.history[i][1])
+            t0 = int(self.history[i-1][1])
+            t1 = int(self.history[i][1])
             
-            dt=(t1.nanoseconds - t0.nanoseconds) / 1e9
-            
-            dt_avg+=dt
+            dt = (t1 - t0) / 1e9
+            dt_avg += dt
 
-            # use constant dt if the messages arrived inconsistent
-            # for example dt=0.1 overwriting the calculation          
+            error_dot += (self.history[i][0] - self.history[i-1][0]) / dt
             
-            # TODO Part 5: calculate the error dot 
-            # error_dot+= ... 
-            
-        error_dot/=len(self.history)
-        dt_avg/=len(self.history)
+        error_dot /= len(self.history) - 1
+        dt_avg /= len(self.history) - 1
         
-        # Compute the error integral
-        sum_=0
-        for hist in self.history:
-            # TODO Part 5: Gather the integration
-            # sum_+=...
-            pass
+        sum_ = sum([val[0] for val in self.history])
+        error_int = sum_ * dt_avg
         
-        error_int=sum_*dt_avg
+        self.logger.log_values([latest_error, error_dot, error_int, latest_error_timestamp])
         
-        # TODO Part 4: Log your errors
-        self.logger.log_values( ... )
-        
-        # TODO Part 4: Implement the control law of P-controller
         if self.type == ControllerType.P:
-            return ... # complete
+            return self.kp * latest_error
         
-        # TODO Part 5: Implement the control law corresponding to each type of controller
         elif self.type == ControllerType.PD:
-            pass
-            # return ... # complete
+            return self.kp * latest_error + self.kv * error_dot
         
         elif self.type == ControllerType.PI:
-            pass
-            # return ... # complete
+            return self.kp * latest_error + self.ki * error_int
         
         elif self.type == ControllerType.PID:
-            pass
-            # return ... # complete
+            return self.kp * latest_error + self.kv * error_dot + self.ki * error_int
